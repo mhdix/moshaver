b39426481cdd9905cdf22466f2f89d970be5a599
mhdix <domanloomu@gmail.com>
2026-09-09 19:33:08 +0330

    add server url to backend cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "http://185.221.237.106"],
         credentials: true,
     })
 );
